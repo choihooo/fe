aab111c93204117b539700533a3ed4847ec764cf
chasyuss <cotn963@gmail.com>
2025-07-07 15:48:29 +0900

    [SSN-92] feat: footer 링크 연결

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,21 +6,43 @@ function Footer() {
     <div className="flex px-[120px] py-[70px] items-start flex-col">
       <div className="w-full justify-between flex flex-row">
         <div className="text-gray-800 font-T01-SB"> 수상한 녀석들</div>
-        <Insta />
+        <a
+          href="https://www.instagram.com/soosanghan.official/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Insta />
+        </a>
       </div>
 
-      <div className="mt-[52px] flex flex-row items-center cursor-pointer">
-        <div className="text-gray-600 font-B01-M hover:text-gray-900">
-          {" "}
-          문의사항
-        </div>
+      <a
+        href="https://forms.gle/CRLShaTW7ECVggKXA"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mt-[52px] flex flex-row items-center cursor-pointer hover:text-gray-900 text-gray-600 font-B01-M"
+      >
+        문의사항
         <TopRightArrow />
-      </div>
+      </a>
 
       <div className="mt-[10px] flex flex-row w-full justify-between">
         <div className="text-gray-800 flex flex-row font-B01-M gap-7 cursor-pointer">
-          <p className="hover:underline"> 개인정보 처리방침</p>
-          <p className="hover:underline"> 서비스 이용약관</p>
+          <a
+            href="https://valley-tenor-1ca.notion.site/223f3199866980549056cc5ff8a4d6c0?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            개인정보 처리방침
+          </a>
+          <a
+            href=" https://valley-tenor-1ca.notion.site/223f31998669801d99cdddfe1b4c4046?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            서비스 이용약관
+          </a>
         </div>
 
         <p className="text-gray-600 font-B01-M">
