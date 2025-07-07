f5eb54364639e8a748db092058968fd51f5714ce
chasyuss <cotn963@gmail.com>
2025-07-08 01:57:49 +0900

    [SSN-100] chore: 아이콘 폴더 위치 변경

---
+++ public/index.ts
@@ -0,0 +1,7 @@
+export { default as LeftArrow } from "./icons/LeftArrow";
+export { default as RightArrow } from "./icons/RightArrow";
+export { default as Star } from "./icons/Star";
+export { default as NoFillStar } from "./icons/StarNoFIll";
+export { default as Insta } from "./icons/Insta";
+export { default as TopRightArrow } from "./icons/TopRightArrow";
+export { default as KaKao } from "./icons/KaKaoIcon";
