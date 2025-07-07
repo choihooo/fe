7d27ec3f54508a866ba952977eef138f2a669dd3
chasyuss <cotn963@gmail.com>
2025-07-07 15:24:36 +0900

    [SSN-92] feat: betareview 구현

---
+++ src/app/assets/icons/LeftArrow.tsx
@@ -0,0 +1,28 @@
+const LeftArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+    >
+      <circle
+        cx="16.6667"
+        cy="16.6667"
+        r="15.5"
+        transform="matrix(-1 0 0 1 36.6658 3.33301)"
+        stroke="#434448"
+        strokeWidth="2.33333"
+      />
+      <path
+        d="M22.1428 13.333L16.5074 18.9685C15.9378 19.538 15.9378 20.4614 16.5074 21.0309L22.1428 26.6663"
+        stroke="#434448"
+        strokeWidth="2.33333"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default LeftArrow;
