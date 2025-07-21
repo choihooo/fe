82a79ff7ae0475dff344650dc52a34de1dff66d8
chasyuss <cotn963@gmail.com>
2025-07-22 00:48:08 +0900

    [SSN-109] chore: 텍스트 수정

--- src/components/common/ApplyCompleteModal.tsx
+++ src/components/common/ApplyCompleteModal.tsx
@@ -23,9 +23,7 @@ const ApplyComfirmModal = ({ isOpen, onClose }: ApplyComfirmModalProps) => {
         </div>
         <div className="flex px-[21px] py-[22px] flex-col justify-center items-start gap-[10px] rounded-[10px] bg-[#F5F5F5] text-gray-400 font-B03-M">
           리포트가 제작되면 입력한 메일로 안내드립니다.
-          <br />
-          현재 제출한 팀원들에게도 메일이 발송됩니다.
-          <br />
+          <div>현재 제출한 팀원들에게도 메일이 발송됩니다.</div>
           팀원들은 로그인 후 리포트 코드로
           <br />
           리포트를 확인할 수 있습니다.
