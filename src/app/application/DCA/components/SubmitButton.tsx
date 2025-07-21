9e711b200105a8c54ed8321b2777cefa5410c3b5
chasyuss <cotn963@gmail.com>
2025-07-22 00:48:41 +0900

    [SSN-109] feat: 리포트 신청 취소 모달 구현

--- src/app/application/DCA/components/SubmitButton.tsx
+++ src/app/application/DCA/components/SubmitButton.tsx
@@ -30,8 +30,9 @@ const SubmitButton = ({ mode }: SubmitButtonProps) => {
 
   const isDisabled = !(isDcaValid || isYccValid);
 
-  const [isModalOpen, setIsModalOpen] = useState(false); // ConfirmModal
-  const [isCompleteModalOpen, setIsCompleteModalOpen] = useState(false); // ✅ SuccessModal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCompleteModalOpen, setIsCompleteModalOpen] = useState(false);
+  const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
 
   const handleSubmitClick = () => {
     if (!isDisabled) {
@@ -49,6 +50,11 @@ const SubmitButton = ({ mode }: SubmitButtonProps) => {
     setIsCompleteModalOpen(false);
   };
 
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    setIsCancelModalOpen(true);
+  };
+
   return (
     <>
       <div className="mt-[130px] flex justify-center mb-[148px] w-full">
@@ -62,7 +68,7 @@ const SubmitButton = ({ mode }: SubmitButtonProps) => {
 
       <ConfirmModal
         isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
+        onClose={handleCancel}
         onConfirm={handleConfirm}
         title="리포트를 신청하시겠습니까?"
         description="신청 후에는 작품을 변경할 수 없습니다."
@@ -73,6 +79,16 @@ const SubmitButton = ({ mode }: SubmitButtonProps) => {
       <ApplyComfirmModal
         isOpen={isCompleteModalOpen}
         onClose={handleCompleteClose}
+      />
+
+      <ConfirmModal
+        isOpen={isCancelModalOpen}
+        onClose={() => setIsCancelModalOpen(false)}
+        onConfirm={() => setIsCancelModalOpen(false)}
+        title="신청을 취소하시겠습니까?"
+        description="현재 입력한 정보는 삭제됩니다."
+        cancelText="취소"
+        confirmText="계속 작성하기"
       />
     </>
   );
