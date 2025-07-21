972dfe8ef37383c0631c037cf09f51db8329956e
choihooo <hochoi8621@naver.com>
2025-07-21 20:26:49 +0900

    [SSN-116] fix: 로그인 스타일 수정

--- src/app/login/_components/Tooltip.tsx
+++ src/app/login/_components/Tooltip.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { CloseIcon, CommentIcon } from "../../../../public";
 
-const ToolTip = ({ text }: { text: string }) => {
+const ToolTip = ({
+  text,
+  onClose,
+}: {
+  text: string;
+  onClose: (e: React.MouseEvent<HTMLDivElement>) => void;
+}) => {
   return (
-    <div className="relative">
+    <div className="relative" onClick={onClose}>
       <CommentIcon />
-
       <div className="absolute inset-0 w-full flex items-center justify-between pl-[17px] pt-2 pb-3 pr-[15px] text-white font-C01-M">
         {text}
-
         <div className="flex items-center">
           <CloseIcon />
         </div>
