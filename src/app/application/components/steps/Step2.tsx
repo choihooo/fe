767b5a929f490bd3e65155b50676b21cb886517e
choihooo <hochoi8621@naver.com>
2025-07-28 22:51:38 +0900

    [SSN-134] feat: 신청하기 AuthGuard 재설정

--- src/app/application/components/steps/Step2.tsx
+++ src/app/application/components/steps/Step2.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StepLayout } from "../common/StepLayout";
 import { StepNavigation } from "../common/StepNavigation";
 import AP03 from "../../lotties/AP03";
+import { AuthGuard } from "@/components/common/AuthGuard";
 
 type Step2Props = {
   onNext: () => void;
@@ -19,21 +20,23 @@ export function Step2({
 }: Step2Props) {
   return (
     <div>
-      <StepLayout
-        stepNumber={stepNumber}
-        totalSteps={totalSteps}
-        title="리포트 완성 메일"
-        subtitle={
-          <div className="pb-[27px]">
-            리포트를 제작한 후, 완료되었다는 메일을 보내드립니다.
+      <AuthGuard>
+        <StepLayout
+          stepNumber={stepNumber}
+          totalSteps={totalSteps}
+          title="리포트 완성 메일"
+          subtitle={
+            <div className="pb-[27px]">
+              리포트를 제작한 후, 완료되었다는 메일을 보내드립니다.
+            </div>
+          }
+        >
+          <div className="rounded-[20px] overflow-hidden">
+            <AP03 />
           </div>
-        }
-      >
-        <div className="rounded-[20px] overflow-hidden">
-          <AP03 />
-        </div>
-      </StepLayout>
-      <StepNavigation onNext={onNext} onPrev={onPrev} />
+        </StepLayout>
+        <StepNavigation onNext={onNext} onPrev={onPrev} />
+      </AuthGuard>
     </div>
   );
 }
--- src/app/application/components/steps/Step3.tsx
+++ src/app/application/components/steps/Step3.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { StepLayout } from "../common/StepLayout";
 import { StepNavigation } from "../common/StepNavigation";
 import AP04 from "../../lotties/AP04";
+import { AuthGuard } from "@/components/common/AuthGuard";
 
 type Step3Props = {
   onPrev: () => void;
@@ -12,38 +13,45 @@ type Step3Props = {
   contest: "daehong" | "hsad";
 };
 
-export function Step3({ onPrev, stepNumber = 3, totalSteps = 3, contest }: Step3Props) {
+export function Step3({
+  onPrev,
+  stepNumber = 3,
+  totalSteps = 3,
+  contest,
+}: Step3Props) {
   const router = useRouter();
 
   return (
     <div>
-      <StepLayout
-        stepNumber={stepNumber}
-        totalSteps={totalSteps}
-        title="이메일 안내"
-        subtitle={
-          <>
-            ‘내 리포트’ 탭에서 완성된 수상 리포트를 확인할 수 있습니다.
-            <br />
-            리포트의 코드를 팀원들에게 전달해 공유할 수 있습니다.
-          </>
-        }
-      >
-        <div className="rounded-[20px] overflow-hidden">
-          <AP04 />
-        </div>
-      </StepLayout>
-
-      <StepNavigation
-        onPrev={onPrev}
-        onNext={() => {
-          if (contest === "daehong") {
-            router.push("/application/DCA");
-          } else {
-            router.push("/application/YCC");
+      <AuthGuard>
+        <StepLayout
+          stepNumber={stepNumber}
+          totalSteps={totalSteps}
+          title="이메일 안내"
+          subtitle={
+            <>
+              ‘내 리포트’ 탭에서 완성된 수상 리포트를 확인할 수 있습니다.
+              <br />
+              리포트의 코드를 팀원들에게 전달해 공유할 수 있습니다.
+            </>
           }
-        }}
-      />
+        >
+          <div className="rounded-[20px] overflow-hidden">
+            <AP04 />
+          </div>
+        </StepLayout>
+
+        <StepNavigation
+          onPrev={onPrev}
+          onNext={() => {
+            if (contest === "daehong") {
+              router.push("/application/DCA");
+            } else {
+              router.push("/application/YCC");
+            }
+          }}
+        />
+      </AuthGuard>
     </div>
   );
 }
