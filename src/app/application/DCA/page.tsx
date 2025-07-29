844eb6129bf3cf1f0f08beb130bb4249ab731e52
chasyuss <cotn963@gmail.com>
2025-07-29 12:02:12 +0900

    [SSN-134] chore: 신청하기 header 추가

--- src/app/application/DCA/page.tsx
+++ src/app/application/DCA/page.tsx
@@ -7,32 +7,36 @@ import VideoLink from "./components/VideoLink";
 import SubmitButton from "./components/SubmitButton";
 import Footer from "@/app/home/components/Footer";
 import Image from "next/image";
+import Header from "@/components/common/Header";
 
 const page = () => {
   return (
-    <div className="mt-[158px] flex flex-col items-start justify-center">
-      <div className="flex flex-row justify-between w-full ">
-        <div className="text-gray-900 font-T01-B px-[120px]">
-          출품작을 업로드하고 <br /> 수상 리포트를 받아보세요
+    <>
+      <Header />
+      <div className="mt-[74px] flex flex-col items-start justify-center">
+        <div className="flex flex-row justify-between w-full ">
+          <div className="text-gray-900 font-T01-B px-[120px]">
+            출품작을 업로드하고 <br /> 수상 리포트를 받아보세요
+          </div>
+          <Image
+            src="/image/application/graphicImg.png"
+            alt="그래픽이미지"
+            width={704}
+            height={128}
+            quality={90}
+            className="w-full sm:max-w-[493px] h-auto"
+          />
         </div>
-        <Image
-          src="/image/application/graphicImg.png"
-          alt="그래픽이미지"
-          width={704}
-          height={128}
-          quality={90}
-          className="w-full sm:max-w-[493px] h-auto"
-        />
-      </div>
 
-      <WorkInformation />
-      <TeamInformation mode="dca" />
-      <BriefBoard />
-      <PlanPart />
-      <VideoLink />
-      <SubmitButton mode="dca" />
-      <Footer />
-    </div>
+        <WorkInformation />
+        <TeamInformation mode="dca" />
+        <BriefBoard />
+        <PlanPart />
+        <VideoLink />
+        <SubmitButton mode="dca" />
+        <Footer />
+      </div>
+    </>
   );
 };
 
