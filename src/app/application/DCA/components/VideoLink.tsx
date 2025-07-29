5e0c737a22950f8248bdee648a75fb24430e916b
chasyuss <cotn963@gmail.com>
2025-07-29 19:05:33 +0900

    [SSN-134] fix: 카테고리 film 일때 선택 라이팅 사라지게 수정

--- src/app/application/DCA/components/VideoLink.tsx
+++ src/app/application/DCA/components/VideoLink.tsx
@@ -6,11 +6,17 @@ import { useSubmitStore } from "@/store/useSubmitStore";
 const VideoLink = () => {
   const youtubeUrl = useSubmitStore((s) => s.youtubeUrl);
   const setField = useSubmitStore((s) => s.setField);
+  const category = useSubmitStore((s) => s.category);
 
   return (
     <div className="w-full px-[325px] mt-[130px]">
       <div className="text-gray-900 font-T02-B mb-6">
-        영상 링크 제출 <span className="text-gray-500 font-T02-SB">(선택)</span>
+        영상 링크 제출{" "}
+        {category === "Film" ? (
+          <span className="text-blue-main">*</span>
+        ) : (
+          <span className="text-gray-500 font-T02-SB">(선택)</span>
+        )}
       </div>
 
       <TextInput
