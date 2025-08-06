3ad15048b2aa24f20ea1f96e0c75404fd610a8cf
choihooo <hochoi8621@naver.com>
2025-08-06 15:15:30 +0900

    [SSN-161] feat: 신청하기 공모전 선택  반응형 구현

--- src/app/application/components/steps/ContestSelectionStep.tsx
+++ src/app/application/components/steps/ContestSelectionStep.tsx
@@ -5,6 +5,7 @@ import { ContestCard } from "../common/ContestCard";
 import ReportIcon from "../../../../../public/icons/ReportIcon";
 import { useAuth } from "@/hooks/queries/useAuth";
 import { LoginModal } from "@/components/common/LoginModal";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 type ContestSelectionStepProps = {
   selectedContest?: "daehong" | "hsad";
@@ -15,6 +16,7 @@ export function ContestSelectionStep({
   selectedContest,
   onNext,
 }: ContestSelectionStepProps) {
+  const isMobile = useIsMobile();
   const [, setSelected] = useState<"daehong" | "hsad" | undefined>(
     selectedContest
   );
@@ -35,24 +37,42 @@ export function ContestSelectionStep({
   return (
     <>
       <div className="flex flex-col gap-[38px]">
-        <div className="flex flex-col gap-[10px]">
-          <div className="flex gap-[10px] font-T01-SB">
-            <ReportIcon />
-            수상 리포트 신청
+        <div className="sm:px-0 px-5 flex flex-col justify-center gap-[10px]">
+          <div
+            className={`flex gap-[10px] sm:mt-0 mt-[34px] ${
+              isMobile ? "font-B01-SB" : "font-T01-SB"
+            }`}
+          >
+            <ReportIcon
+              width={isMobile ? 21 : 31}
+              height={isMobile ? 24 : 35}
+            />
+            수상리포트 신청
           </div>
-          <div className="text-gray-600 font-B01-M">
+          <div
+            className={`text-gray-600 ${
+              isMobile ? "font-B03-R" : "font-B01-M"
+            }`}
+          >
             출품하신 공모전을 선택해주세요
           </div>
         </div>
-        <div className="flex gap-[30px]">
-          <ContestCard
-            type="daehong"
-            onClick={() => handleContestSelect("daehong")}
-          />
-          <ContestCard
-            type="hsad"
-            onClick={() => handleContestSelect("hsad")}
-          />
+
+        <div className="overflow-hidden sm:overflow-visible w-screen sm:w-full sm:h-full h-[calc(100vh-170px)] bg-gray-100">
+          <div
+            className={`flex gap-[30px] ${
+              isMobile ? "overflow-x-auto scrollbar-hide h-full px-[20px] pt-[54px]" : ""
+            }`}
+          >
+            <ContestCard
+              type="daehong"
+              onClick={() => handleContestSelect("daehong")}
+            />
+            <ContestCard
+              type="hsad"
+              onClick={() => handleContestSelect("hsad")}
+            />
+          </div>
         </div>
       </div>
 
--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -6,9 +6,9 @@ import Header from "@/components/common/Header";
 
 export default function Application() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-white sm:bg-gray-100">
       <Header />
-      <div className="flex justify-center items-center min-h-[calc(100vh-80px)]">
+      <div className="flex sm:justify-center sm:items-center min-h-[calc(100vh-50px)]">
         <ApplicationFunnel />
       </div>
     </div>
